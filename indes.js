f888a512cbcb89cf0c4c1c7a10e053c4cc84ed71
AlonsodvJunior <79492556+AlonsodvJunior@users.noreply.github.com>
2024-12-05 14:42:00 -0500

    Actualizar indes.js

--- indes.js
+++ indes.js
@@ -19,12 +19,22 @@ let filterCards = [];
 let btnsCreated = [];
 
 
+function x (  ) {
+  
+}
+
 function () {
   const totalPages;
   
   if ( filterCards.length >= 6 ) {
+
     totalPages = filterCards.length / 6;
+
   } else {
+
     totalPages = 1;
+  
   }
+
+  x ( totalPages )
 }
